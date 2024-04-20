b4db9b82aa4209700d3982d9cb4f39f69028f2dd
Erik Řehulka <erik.rehulka@bratislava.sk>
2024-04-20 16:26:15 +0200

    get ids of all matches 2 days ago

---
+++ getMatchesFlashscoreScript.py
@@ -0,0 +1,56 @@
+import re
+import time
+from selenium import webdriver # Selenium needed because of need to click buttons on the web
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+
+# Configure browser options
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
+
+driver = webdriver.Chrome(options=options)
+
+# Navigate to the URL
+url = "https://www.flashscore.sk/futbal"
+driver.get(url)
+
+# Dismiss any cookie consent or privacy notifications
+try:
+    cookie_consent = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "onetrust-button-group")))
+    cookie_consent.click()
+    time.sleep(10)
+except:
+    print("Cookie consent not found or error dismissing it")
+
+# Click the "yesterday" button twice
+try:
+    yesterday_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "calendar__navigation--yesterday")))
+    for _ in range(2):
+        yesterday_button.click()
+        time.sleep(5)
+except Exception as e:
+    print("Error clicking yesterday button")
+    print(e)
+    exit(1)
+
+time.sleep(10)
+
+html = driver.page_source
+parsed = BeautifulSoup(html, 'html.parser')
+
+
+matchIds: list[str] = []
+matches = parsed.select('.event__match')
+for match in matches:
+    matchId = match.attrs['id'] # For example g_1_faTuORtF
+    matchIdParsed = re.sub(r'^g_1_(.*)$', r'\1', matchId) # We need to get only faTuORtF
+    matchIds.append(matchIdParsed)
+
+# TODO - open each match and parse the data
+
+driver.quit()
