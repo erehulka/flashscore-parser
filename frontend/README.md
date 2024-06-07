0cc97818fe866a44917fbd4acc3d1579c506c465
Erik Řehulka <erik.rehulka@bratislava.sk>
2024-06-07 20:37:02 +0200

    continue with frontend

---
+++ frontend/README.md
@@ -0,0 +1,7 @@
+To run frontend:
+
+```
+export FLASK_APP=main.py
+export FLASK_ENV=development 
+flask run --port=3001
+```
--- frontend/main.py
+++ frontend/main.py
@@ -30,7 +30,7 @@ def home():
                     FROM teamMatchStats tms
                     WHERE tms.expectedGoals IS NOT NULL and tms.expectedGoals > 0
                    """)
-    avgEffectivity = cursor.fetchone()[0]
+    avgEffectivity = round(cursor.fetchone()[0], 2)
 
     tenDaysAgo = (datetime.today() - timedelta(days=10)).strftime('%Y-%m-%d')
 
@@ -53,7 +53,7 @@ def home():
 
 @app.route('/league/<league_id>')
 def league(league_id):
-    cursor = g.db.cursor()
+    cursor: sqlite3.Cursor = g.db.cursor()
     cursor.execute("""
                    SELECT DISTINCT t.id, t.name FROM teams t 
                    JOIN teamMatchStats tms ON tms.team = t.id 
@@ -65,22 +65,63 @@ def league(league_id):
     for row in cursor:
         teams.append(row)
 
-    return render_template('league.html', teams=teams, league_id=league_id)
+    cursor.execute("""
+                   SELECT urlName, country, name FROM leagues WHERE id = ?
+                   """, (league_id,))
+    league_info = cursor.fetchone()
+
+    return render_template('league.html', teams=teams, league_id=league_id, league_info=league_info)
 
 @app.route('/team/<league_id>/<team_id>')
 def team(league_id, team_id):
     cursor = g.db.cursor()
     cursor.execute("""
-                   SELECT m.matchId
+                   SELECT m.matchId, tHome.name, tAway.name, tmsHome.goals, tmsAway.goals
                    FROM matches m
-                   JOIN teamMatchStats tms ON tms.id = m.homeTeam OR tms.id = m.awayTeam
-                   JOIN teams t ON t.id = tms.team
-                   WHERE t.id = ?
+                   JOIN teamMatchStats tmsHome ON tmsHome.id = m.homeTeam
+                   JOIN teamMatchStats tmsAway ON tmsAway.id = m.awayTeam
+                   JOIN teams tHome ON tHome.id = tmsHome.team
+                   JOIN teams tAway ON tAway.id = tmsAway.team
+                   WHERE tHome.id = ? OR tAway.id = ?
                    ORDER BY m.dateTime DESC
                    LIMIT 5
-                   """, (team_id,))
+                   """, (team_id, team_id))
     last5Matches = []
     for row in cursor:
-        last5Matches.append(row)
+        last5Matches.append({
+            "matchId": row[0],
+            "homeTeam": row[1],
+            "awayTeam": row[2],
+            "homeGoals": row[3],
+            "awayGoals": row[4],
+        })
 
-    return render_template('team.html', last5Matches=last5Matches, league_id=league_id)
+    tenDaysAgo = (datetime.today() - timedelta(days=10)).strftime('%Y-%m-%d')
+    cursor.execute(
+        """
+            SELECT SUBSTR(m.dateTime, 1, 10) as date, AVG(tms.goals / tms.expectedGoals) as avg_effectivity
+            FROM teamMatchStats tms
+            JOIN matches m ON m.homeTeam = tms.id OR m.awayTeam = tms.id
+            WHERE date >= ? AND tms.expectedGoals IS NOT NULL and tms.expectedGoals > 0 AND tms.team = ?
+            GROUP BY date
+            ORDER BY date
+        """,
+        (tenDaysAgo, team_id)
+    )
+    avgEffectivityPerDay = {}
+    for row in cursor:
+        avgEffectivityPerDay[row[0]] = row[1]
+
+    cursor.execute("""
+                    SELECT AVG(tms.goals / tms.expectedGoals) as avg_effectivity
+                    FROM teamMatchStats tms
+                    WHERE tms.expectedGoals IS NOT NULL and tms.expectedGoals > 0 AND tms.team = ?
+                   """, (team_id,))
+    avgEffectivity = round(cursor.fetchone()[0] or 0, 2)
+
+    cursor.execute("""
+                   SELECT urlName, name FROM teams WHERE id = ?
+                   """, (team_id,))
+    team_info = cursor.fetchone()
+
+    return render_template('team.html', last5Matches=last5Matches, league_id=league_id, avgEffectivityPerDay=avgEffectivityPerDay, avgEffectivity=avgEffectivity, team_info=team_info)
